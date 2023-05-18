ce87f3f971146850156805870b1be6caeeef7608
Stegonyrob <127294546+Stegonyrob@users.noreply.github.com>
2023-05-18 20:09:29 +0200

    e

--- keybook/Frontend/src/Components/NavBar.js
+++ keybook/Frontend/src/Components/NavBar.js
@@ -22,86 +22,6 @@ export default function NavBar() {
   // toggleMenu es una función que se utiliza para cambiar el valor de isMenuOpen.
   // Si isMenuOpen es true, se cambia a false, y si es false, se cambia a true.
 
-
-
-
-  import { useState } from "react";
-  import GrayScaleButton from "./GrayScaleButton";
-  import CustomIcon from "./Icons";
-  import SearchBar from "./SearchBar";
-  
-  export default function NavBar() {
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
-  
-    const toggleMenu = () => {
-      setIsMenuOpen(!isMenuOpen);
-    };
-  
-    return (
-      <>
-        <nav className="navbar navbar-expand-lg">
-          <div className="container-fluid">
-            <div className="row nav-body">
-              <div className="col">
-                <a className="navbar-brand" href="home.html">
-                  <img
-                    src="../img/logo2.png"
-                    alt="Logo"
-                    className="keybook-logo d-block"
-                  />
-                </a>
-              </div>
-              <SearchBar placeholder="Búsqueda..." />
-              <div className="col-3 col-sm-1">
-
-  {/* Hamburger dropdown */}
-
-                <button
-                  className={`navbar-toggler ${isMenuOpen ? "girar" : ""}`}
-                  type="button"
-                  onClick={toggleMenu}
-                >
-                  <span className={`br-1 ${isMenuOpen ? "animado" : ""}`}></span>
-                  <span className={`br-2 ${isMenuOpen ? "animado" : ""}`}></span>
-                  <span className={`br-3 ${isMenuOpen ? "animado" : ""}`}></span>
-                </button>
-              </div>
-              <div className="col-1"></div>
-              <div
-                className={`collapse navbar-collapse col-sm-7 col-lg-6 ${
-                  isMenuOpen ? "show" : ""
-                }`}
-                id="navbarNav"
-              >
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
   return (
     /*BARRA DE NAVEGACIÓN*/
     <>
@@ -162,6 +82,7 @@ export default function NavBar() {
                 className="overlay"
                 style={{ display: isMenuOpen ? "block" : "none" }}
               ></div>
+
               <ul className="navbar-nav me-auto mb-2 mb-lg-0 nav-icons">
                 {/*  Mensajes*/}
                 <li className="nav-item">
