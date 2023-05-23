df479f460fd4548c020fff44ffc0208540c3c7bd
Lu <luciasaenz92@hotmail.com>
2023-05-23 13:48:39 +0200

    v1

--- keybook/Backend/routes/friends.js
+++ keybook/Backend/routes/friends.js
@@ -16,4 +16,18 @@ router.get('/', async function (req, res, next) {
     }
 });
 
+//Friend request
+router.get('/request', async function (req, res, next) {
+    const receptor_id = req.params.user_id
+    try {
+        const friendRequest = await sequelize.query(
+            "SELECT friend.receptor_id, user.id, user.name FROM user INNED JOIN friend ON friend.sender_id = user.id WHERE friend.receptor_id = ? AND friend.status = ?",
+            [receptor_id, FRIEND_STATUS.PENDING])
+        res.status(200).send(friendRequest);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Error interno del servidor");
+    }
+});
+
 module.exports = router;
