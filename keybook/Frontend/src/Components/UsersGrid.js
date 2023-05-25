a5b57ab11285d71b817ab66c19848958a2dfe353
A.Page <58567281+Tenses@users.noreply.github.com>
2023-05-25 20:49:25 +0200

    prueba 1

--- keybook/Frontend/src/Components/UsersGrid.js
+++ keybook/Frontend/src/Components/UsersGrid.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import SearchBarUsers from './SearchBarUsers';
 
 function UsersGrid() {
@@ -7,7 +8,6 @@ function UsersGrid() {
     const [searchKey, setSearchKey] = useState('');
 
     useEffect(() => {
-        // Lógica para obtener todos los usuarios al cargar la página
         const fetchUsers = async () => {
             try {
                 const response = await fetch('http://localhost:3000/users');
@@ -35,7 +35,6 @@ function UsersGrid() {
 
     return (
         <main>
-            {/* Menú búsqueda amigos */}
             <div className="container main-structure">
                 <div className="row">
                     <div className="col-sm-12 col-md-6 col-lg-6">
@@ -56,18 +55,18 @@ function UsersGrid() {
                 </div>
             </div>
 
-            {/* Menú amigos */}
             <div className="container main-structure">
                 <article className="row friends-row">
-                    {/* para renderizar los resultados de búsqueda en el articulo */}
                     {filteredResults.map((userData) => (
                         <div className="col-sm-3 default-card friend-box" key={userData.id}>
-                            <img
-                                className="friend-avatar"
-                                style={{ borderRadius: '50%', width: '150px', height: '150px' }}
-                                src={userData.profile_picture}
-                                alt={userData.name}
-                            />
+                            <Link to={{ pathname: '/profile', state: { user: userData } }}>
+                                <img
+                                    className="friend-avatar"
+                                    style={{ borderRadius: '50%', width: '150px', height: '150px' }}
+                                    src={userData.profile_picture}
+                                    alt={userData.name}
+                                />
+                            </Link>
                             <a>{userData.name}</a>
                             <p>{userData.email}</p>
                             <button className="btn btn-outline-warning btn-sm">Enviar solicitud de Amistad</button>
@@ -78,4 +77,5 @@ function UsersGrid() {
         </main>
     );
 }
+
 export default UsersGrid;
--- keybook/Frontend/src/Components/profile/Profile.js
+++ keybook/Frontend/src/Components/profile/Profile.js
@@ -1,36 +1,51 @@
 import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import ProfileCard from './ProfileCard';
 import ProfileHobbies from './ProfileHobbies';
 import ProfileStudies from './ProfileStudies';
 import ProfileLanguages from './ProfileLanguages';
 import ProfileTools from './ProfileTools';
 
-function Profile() {
+function Profile(props) {
   const [user, setUser] = useState();
 
   useEffect(() => {
     const fetchUser = async () => {
-      const userId = localStorage.getItem('userId');
+      const userId = props.match.params.userId || localStorage.getItem('userId');
       const response = await fetch(`http://localhost:3000/users/user/${userId}`);
       const data = await response.json();
       setUser(data);
     };
 
     fetchUser();
-  }, []);
+  }, [props.match.params.userId]);
+
+  // function Profile() {
+  //   const [user, setUser] = useState();
+
+  //   useEffect(() => {
+  //     const fetchUser = async () => {
+  //       const userId = localStorage.getItem('userId');
+  //       const response = await fetch(`http://localhost:3000/users/user/${userId}`);
+  //       const data = await response.json();
+  //       setUser(data);
+  //     };
+
+  //     fetchUser();
+  //   }, []);
 
   return (
     <div id="cuenta">
-      <div class="container-fluid main-structure">
-        <div class="row">
-          <div class="col-sm-5 col-md-4 col-lg-3">
+      <div className="container-fluid main-structure">
+        <div className="row">
+          <div className="col-sm-5 col-md-4 col-lg-3">
             {user && <ProfileStudies userId={user.id} />}
             {user && <ProfileTools userId={user.id} />}
           </div>
-          <div class="col-sm-7 col-md-7 col-lg-6">
+          <div className="col-sm-7 col-md-7 col-lg-6">
             {user && <ProfileCard userId={user.id} />}
           </div>
-          <div class="col-sm-5 col-md-4 col-lg-3">
+          <div className="col-sm-5 col-md-4 col-lg-3">
             {user && <ProfileHobbies userId={user.id} />}
             {user && <ProfileLanguages userId={user.id} />}
           </div>
@@ -39,6 +54,5 @@ function Profile() {
     </div>
   );
 }
-
 
 export default Profile;
