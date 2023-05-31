3fbb5f490c8c3dd2e77e5ef78995f082efe7ef6d
Stegonyrob <127294546+Stegonyrob@users.noreply.github.com>
2023-05-31 13:49:42 +0200

    nos eguidos funcionando

--- keybook/Frontend/src/Components/home/RightSidebar.js
+++ keybook/Frontend/src/Components/home/RightSidebar.js
@@ -10,7 +10,7 @@ function RightSidebar({ loggedUserId }) {
   async function fetchRequests() {
     try {
       const response = await fetch(
-        `fetch("http://localhost:3000/follow/not-following/${loggedUserId}`
+        `http://localhost:3000/follow/not-following/${loggedUserId}`
       );
       const data = await response.json();
       setRequests(data);
@@ -21,7 +21,7 @@ function RightSidebar({ loggedUserId }) {
   }
   useEffect(() => {
     fetchRequests();
-  }, [page]);
+  }, [page, loggedUserId]);
 
   return (
     <div className="default-card-right">
@@ -47,41 +47,3 @@ function RightSidebar({ loggedUserId }) {
   );
 }
 export default RightSidebar;
-// import React, { useState, useEffect } from "react";
-// import Unfollow from "../buttons/UnfollowButton";
-
-// function LeftSidebar() {
-//   const [users, setUsers] = useState([]);
-//   const userId = localStorage.getItem('userId');
-
-//   async function fetchUsers() {
-//     try {
-//       const response = await fetch(
-//         `http://localhost:3000/follow/following/${userId}`
-//       );
-//       const data = await response.json();
-//       setUsers(data);
-//     } catch (error) {
-//       console.error(error);
-//     }
-//   }
-//   useEffect(() => {
-//     fetchUsers();
-//   }, []);
-
-//   return (
-//     <div className="default-card-left ">
-//       <h2>SIGUIENDO</h2>
-//       {users.map((user) => (
-//         <div key={user.id}>
-//           <a title={`Perfil ${user.name}`} href={`/profile/${user.id}`}>
-//             <img src={user.profile_picture} alt="Avatar" className="avatar" /></a>
-//          <h4>{user.name} {user.last_name}</h4>
-//             <Unfollow id={user.id} />
-//         </div>
-//       ))}
-//     </div>
-//   );
-// }
-
-// export default LeftSidebar;
